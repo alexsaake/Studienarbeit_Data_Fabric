d5f7a4a523481d84ac0f4c8ec9362dbcda4a6c6c
Markus <facklerma90911@th-nuernberg.de>
2023-02-02 14:42:51 +0000

    Squashed commit of the following
    commit 3357525d 
    Author: Markus <facklerma90911@th-nuernberg.de> 
    Date: Wed Feb 01 2023 17:25:35 GMT+0100 (Mitteleuropäische Normalzeit) 
    
        please approve

--- FE/store/index.js
+++ FE/store/index.js
@@ -1,21 +1,28 @@
+import https from "https";
 import Vuex from "vuex";
 import axios from "axios";
+
+const axiosInstance = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
+});
 
 const createStore = () => {
   return new Vuex.Store({
     state: () => ({
-      data: []
+      testData: []
     }),
     mutations: {
-      SET_DATA (state, data) {
-        state.data = data
+      SET_TESTDATA (state, testData) {
+        state.testData = testData
       }
     },
     actions: {
       async fetchData ({ commit }) {
         try {
-          const { data } = await axios.get('http://localhost:3000/data.json')
-          commit('SET_DATA', data)
+          const { data } = await axiosInstance.get('https://127.0.0.1:8443/api/Gateway/DataProducts');
+          commit('SET_TESTDATA', data)
         } catch (error) {
           console.error(error)
         }
