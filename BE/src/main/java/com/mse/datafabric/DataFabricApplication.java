bd90997dd561fbecbc94f06ed5b9a1c186c50b18
alexander_scheffler <scheffleral86885@th-nuernberg.de>
2023-02-27 15:12:29 +0100

    extra image endpoint + auto db start and population

--- BE/src/main/java/com/mse/datafabric/DataFabricApplication.java
+++ BE/src/main/java/com/mse/datafabric/DataFabricApplication.java
@@ -1,11 +1,7 @@
 package com.mse.datafabric;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class DataFabricApplication {
