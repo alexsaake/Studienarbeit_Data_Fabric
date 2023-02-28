c129da53385621eb234328fb884b9e606f6885e0
Markus Fackler <markus-fackler@gmx.de>
2023-02-28 20:40:46 +0000

    Squashed commit of the following
    commit 9ff8c081 
    Author: Markus <facklerma90911@th-nuernberg.de> 
    Date: Wed Feb 08 2023 18:08:06 GMT+0100 (Mitteleuropäische Normalzeit) 
    
        DateTime for last Updated

--- FE/pages/marketplace.vue
+++ FE/pages/marketplace.vue
@@ -1,26 +1,5 @@
 <template>
-   <v-container>
-        <v-row>
-          <v-col cols="12" md="6">
-            <v-text-field v-model="search" label="Search"></v-text-field>
-          </v-col>
-          <v-col cols="12" md="6">
-            <v-select v-model="filter" :items="filters" label="Filter"></v-select>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col v-for="item in filteredItems" :key="item.id" cols="12" md="4">
-            <v-card>
-              <v-card-title>{{ item.name }}</v-card-title>
-              <v-card-text>{{ item.description }}</v-card-text>
-              <v-card-actions>
-                <v-btn text @click="addToCart(item)">View Product</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-</template>
+  <v-container>
     <v-row>
       <v-col cols="12" md="6">
         <v-text-field v-model="search" label="Search"></v-text-field>
@@ -53,24 +32,37 @@
   </v-container>
 </template>
 <script>
+import { mapState } from 'vuex'
+import { DateTime } from 'luxon'
 export default {
   name: 'InspirePage',
+  async asyncData({ store }) {
+    await store.dispatch('fetchData')
+  },
   data() {
     return {
       search: '',
       filter: '',
       filters: ['All', 'Category 1', 'Category 2'],
       items: [],
+      cart: [],
     }
   },
   computed: {
     filteredItems() {
       if (this.filter === '' || this.filter === 'All') {
-        return this.items.filter(item => item.name.toLowerCase().includes(this.search.toLowerCase()))
+        return this.items.filter((item) =>
+          item.name.toLowerCase().includes(this.search.toLowerCase())
+        )
       } else {
-        return this.items.filter(item => item.name.toLowerCase().includes(this.search.toLowerCase()) && item.category === this.filter)
+        return this.items.filter(
+          (item) =>
+            item.name.toLowerCase().includes(this.search.toLowerCase()) &&
+            item.category === this.filter
+        )
       }
-    }
+    },
+    ...mapState(['testData']),
   },
   created() {
     this.items = [
@@ -80,7 +72,9 @@ export default {
         description: this.testData[0].shortDescription,
         image: this.testData[0].image,
         accessRights: this.testData[0].accessRights,
-        lastUpdated: this.testData[0].lastUpdated,
+        lastUpdated: DateTime.fromMillis(this.testData[0].lastUpdated).toFormat(
+          'dd.LL.yyyy'
+        ),
         category: 'Category 1',
       },
       {
@@ -106,7 +100,7 @@ export default {
   methods: {
     addToCart(item) {
       this.cart.push(item)
-    }
-  }
+    },
+  },
 }
 </script>
