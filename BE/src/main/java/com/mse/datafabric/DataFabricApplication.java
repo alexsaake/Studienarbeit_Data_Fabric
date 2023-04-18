a96c154368d776d022e3b8ca568d86056459d14c
alexander_scheffler <scheffleral86885@th-nuernberg.de>
2023-04-18 18:55:29 +0200

    MDF-85 Adapter für Abruf von festgelegten Hausinfos von einer WG-Suche Seite

--- BE/src/main/java/com/mse/datafabric/DataFabricApplication.java
+++ BE/src/main/java/com/mse/datafabric/DataFabricApplication.java
@@ -2,8 +2,10 @@ package com.mse.datafabric;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DataFabricApplication {
 
 	public static void main(String[] args) {
--- BE/src/main/java/com/mse/datafabric/immobilien/ImmobilienTimer.java
+++ BE/src/main/java/com/mse/datafabric/immobilien/ImmobilienTimer.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.stereotype.Component;
+
 @ShellComponent
 public class ImmobilienTimer {
 
