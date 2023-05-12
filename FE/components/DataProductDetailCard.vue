d9847dafa25192f0ded6af105ab99190dd926725
Markus <facklerma90911@th-nuernberg.de>
2023-05-12 18:25:22 +0200

    Rest API Link Relative + Loading Circles

--- FE/components/DataProductDetailCard.vue
+++ FE/components/DataProductDetailCard.vue
@@ -1,37 +1,49 @@
 <template>
   <v-card id="dataProductDetail">
-    <v-img
-      :src="'data:text/plain;base64,' + dataProductDetail.image"
-      height="300px"
-    />
-    <v-card-title style="word-break: break-word">{{
-      dataProductDetail.title
-    }}</v-card-title>
-    <v-card-subtitle>{{ dataProductDetail.shortDescription }}</v-card-subtitle>
-    <v-card-text>
-      {{ dataProductDetail.description }}<br />
-      Quelle: {{ dataProductDetail.source }}<br />
-      Quellen-Link: <v-card>{{ dataProductDetail.sourceLink }}</v-card> Zuletzt
-      aktualisiert: {{ dataProductDetail.lastUpdated }}<br />
-      Kategorie: {{ dataProductDetail.category }}<br />
-      Zugriff: {{ dataProductDetail.accessRight }}
-    </v-card-text>
-    <v-card-actions>
-      <v-btn class="mb-4 ml-4" outlined @click="showOverlay = true">
-        Datenprodukt abrufen
-      </v-btn>
-    </v-card-actions>
-    <v-overlay :value="showOverlay" :opacity="1">
-      <a
-        style="color: white"
-        target="_blank"
-        :href="'https://localhost:8443/api/Gateway/DataProducts/' + shortKey"
-        >https://localhost:8443/api/Gateway/DataProducts/{{ shortKey }}</a
-      >
-      <v-layout justify-center>
-        <v-btn class="mt-2" @click="showOverlay = false"> Zurück </v-btn>
-      </v-layout>
-    </v-overlay>
+    <template v-if="!dataProductDetail">
+      <v-progress-circular
+        :size="120"
+        indeterminate
+        color="white"
+      ></v-progress-circular>
+    </template>
+    <template v-else>
+      <v-img
+        :src="'data:text/plain;base64,' + dataProductDetail.image"
+        height="300px"
+      />
+      <v-card-title style="word-break: break-word">{{
+        dataProductDetail.title
+      }}</v-card-title>
+      <v-card-subtitle>{{
+        dataProductDetail.shortDescription
+      }}</v-card-subtitle>
+      <v-card-text>
+        {{ dataProductDetail.description }}<br />
+        Quelle: {{ dataProductDetail.source }}<br />
+        Quellen-Link:
+        <v-card>{{ dataProductDetail.sourceLink }}</v-card> Zuletzt
+        aktualisiert: {{ dataProductDetail.lastUpdated }}<br />
+        Kategorie: {{ dataProductDetail.category }}<br />
+        Zugriff: {{ dataProductDetail.accessRight }}
+      </v-card-text>
+      <v-card-actions>
+        <v-btn class="mb-4 ml-4" outlined @click="showOverlay = true">
+          Datenprodukt abrufen
+        </v-btn>
+      </v-card-actions>
+      <v-overlay :value="showOverlay" :opacity="1">
+        <a
+          style="color: white"
+          target="_blank"
+          :href="'/api/Gateway/DataProducts/' + shortKey"
+          >Datenprodukt via RESTful API abrufen</a
+        >
+        <v-layout justify-center>
+          <v-btn class="mt-2" @click="showOverlay = false"> Zurück </v-btn>
+        </v-layout>
+      </v-overlay>
+    </template>
   </v-card>
 </template>
 
@@ -50,25 +62,24 @@ export default {
   data() {
     return {
       showOverlay: false,
-      dataProductDetail: {
-        shortKey: String,
-        title: String,
-        shortDescription: String,
-        description: String,
-        source: String,
-        sourceLink: String,
-        lastUpdated: Date,
-        category: String,
-        accessRight: String,
-        image: Uint8Array,
-      },
+      dataProductDetail: null, // Initialize to null to indicate data is not yet loaded
     }
   },
   async fetch() {
     this.dataProductDetail = await this.fetchDataProductDetail(this.shortKey)
   },
   watch: {
-    shortKey: '$fetch',
+    shortKey() {
+      this.dataProductDetail = null // Reset to null when shortKey changes to indicate data is not yet loaded
+    },
+    async dataProductDetail() {
+      // Load data when dataProductDetail changes from null to an object
+      if (this.dataProductDetail === null) {
+        this.dataProductDetail = await this.fetchDataProductDetail(
+          this.shortKey
+        )
+      }
+    },
   },
   fetchOnServer: false,
   methods: {
