5aac54c392e5605b1fa33db99ad2c26f93935de8
schne <schneiderha75561@th-nuernberg.de>
2023-05-27 10:23:59 +0200

    Bla

---
+++ BE/src/main/java/com/mse/datafabric/immobilien/webScraper/immoscout24De/PageImmoscout24De.java
@@ -0,0 +1,96 @@
+package com.mse.datafabric.immobilien.webScraper.immoscout24De;
+
+import com.mse.datafabric.immobilien.webScraper.ScrapingPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+
+public class PageImmoscout24De extends ScrapingPage {
+
+    public PageImmoscout24De(String startWebsiteUrl, String domQueryInputElement, String domSubmitFormElement, String domSubmitBtnElement, String pageGetParam) {
+        super(startWebsiteUrl, domQueryInputElement, domSubmitFormElement, domSubmitBtnElement, pageGetParam);
+
+    }
+
+    @Override
+    public String getCityWebsiteUrl(String city) {
+        driver.get(startWebsiteUrl);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String script = "return document.getElementById('usercentrics-root').shadowRoot.querySelector('button[data-testid=uc-accept-all-button]')";
+        WebElement cookieBanner = (WebElement)((JavascriptExecutor)driver).executeScript(script);
+        cookieBanner.click();
+
+        WebElement searchBar = getElementBy(By.id(domQueryInputElement));
+        if (searchBar == null)
+            return null;
+        searchBar.sendKeys(city);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        WebElement searchButton = getElementBy(By.xpath("//form[@id='"+domSubmitFormElement+"']/article/div/div/button"));
+        if (searchButton == null)
+            return null;
+        searchButton.click();
+        searchButton.click();
+
+        do{
+            WebElement captcha = getElementBy(By.xpath("//*[@class='geetest_radar_tip']"));
+            if (searchButton == null)
+                return null;
+            captcha.click();
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }while(getElementBy(By.xpath("//*[@class='geetest_radar_tip']"))!=null);
+
+
+        return driver.getCurrentUrl();
+    }
+
+    public void afterBrowserInit(){
+
+    }
+
+    @Override
+    public List<String> getCityItemUrls(String cityWebsiteUrl) {
+        driver.get(cityWebsiteUrl);
+        
+        return getElementsAttributes(By.xpath("//wgs-search-offer-list-item/div/a"),"href");
+    }
+
+    @Override
+    public String getItemDomContent(String itemUrl) {
+        driver.get(itemUrl);
+        getElementBy(By.className("odp-dataset"));
+
+        return getElementAttribute(By.cssSelector("body"),"innerHTML");
+    }
+    @Override
+    public String getItemIdForUrl(String url){
+        String[] splitUrl = url.split("/");
+        if (splitUrl.length <= 1)
+            return null;
+        return splitUrl[splitUrl.length-2];
+    }
+
+    @Override
+    public String getNextPageUrl(int pageCount){
+        return cityWebsiteUrl+"?"+pageGetParam+"="+pageCount;
+    }
+}
---
+++ BE/src/main/java/com/mse/datafabric/immobilien/webScraper/wgGesuchtDE/DomWgGesuchtDe.java
@@ -0,0 +1,71 @@
+package com.mse.datafabric.immobilien.webScraper.wgGesuchtDE;
+
+
+import com.mse.datafabric.immobilien.webScraper.ScrapingDom;
+import org.jsoup.select.Elements;
+
+public class DomWgGesuchtDe extends ScrapingDom {
+    public DomWgGesuchtDe(String domContent, String itemId, String city) {
+        super(domContent, itemId, city);
+    }
+
+    public String parseContent(String content, boolean split)    {
+        String[] splitContent = content.split("\n");
+        if(!split){
+            String returnContent = "";
+            for(int sc = 0;sc < splitContent.length; sc++){
+                returnContent += splitContent[sc].trim();
+                if (splitContent[sc].trim() != "" && splitContent.length > (sc+1) && splitContent[sc+1].trim() != "")
+                    returnContent+=" ";
+            }
+            return returnContent;
+        }
+        if (splitContent.length > 2){
+            return splitContent[2].trim();
+        }
+        if (splitContent.length > 1){
+            return splitContent[1].trim();
+        }
+        return null;
+    }
+    public String getContentByPathX(String pathXContent, boolean split){
+        Elements elementContent = domContentDocument.select(pathXContent);
+        if(elementContent.size()==0)
+           return null;
+        return parseContent(elementContent.get(0).wholeText(),split);
+    }
+    @Override
+    public String getPortalId(){
+        return "wgSuche";
+    }
+    @Override
+    public String getFlatSize() {return getContentByPathX("div[class='odp-dataset__content']>div[ng-if='offer.flatSize']",true);}
+    @Override
+    public String getRent(){return getContentByPathX("div[class='odp-dataset__content']>div[ng-if='offer.rent']",true);}
+    @Override
+    public String getExtraCharges(){return getContentByPathX("div[class='odp-dataset__content']>div[ng-if='offer.extraCharges']",true);}
+    @Override
+    public String getRoomSize(){return getContentByPathX("div[class='odp-dataset__content']>div[ng-if='offer.size']",true);}
+    @Override
+    public String getDeposit(){return getContentByPathX("div[class='odp-dataset__content']>div[ng-if='offer.deposit']",true);}
+    @Override
+    public String getFrom(){
+        String from1 = getContentByPathX("div[class='odp-dataset__content']>div>span[ng-if='offer.from']",false);
+        String from2 = getContentByPathX("div[class='odp-dataset__content']>div>span[ng-if='!offer.from']",false);
+        if (from1 != null)
+            return from1;
+        else
+            return from2;
+    }
+    @Override
+    public String getBorough(){return null;}
+    @Override
+    public String getFurnished(){
+        return getContentByPathX("div[class='odp-dataset__content']>div>span[translate^='userinput.offer.room.furnishedtype']",false);}
+    @Override
+    public String getStatus(){return null;}
+    @Override
+    public String getCreationDate(){return null;}
+    @Override
+    public String getTitle(){return getContentByPathX("div[class='odp-title']>h1",false);}
+}
