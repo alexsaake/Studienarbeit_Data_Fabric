aa3709cf1972630e1e14a120f986fb086f3f971f
Alexander Scheffler <scheffleralex@web.de>
2023-05-27 09:05:38 +0000

    Squashed commit of the following
    commit 7959bd31 
    Author: Markus <Markus> 
    Date: Fri May 12 2023 18:38:32 GMT+0200 (Mitteleuropäische Sommerzeit) 
    
        Conflicts resolved in the console.
    
    commit d9847daf 
    Author: Markus <facklerma90911@th-nuernberg.de> 
    Date: Fri May 12 2023 18:25:22 GMT+0200 (Mitteleuropäische Sommerzeit) 
    
        Rest API Link Relative + Loading Circles
    
    
    commit 3af4247c 
    Author: Markus <facklerma90911@th-nuernberg.de> 
    Date: Fri Apr 07 2023 21:51:30 GMT+0200 (Mitteleuropäische Sommerzeit) 
    
        impressum and datenschutz added
    
    
    commit 36dcf492 
    Author: Markus <facklerma90911@th-nuernberg.de> 
    Date: Fri Mar 31 2023 18:24:47 GMT+0200 (Mitteleuropäische Sommerzeit) 
    
        Datenprodukt als URL abrufbar

--- FE/pages/marketplace.vue
+++ FE/pages/marketplace.vue
@@ -9,96 +9,137 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col v-if="dataProductsOverview.length > 0" cols="12" md="4">
-        <v-card v-for="dataProductOverview in filteredDataProductsOverview" :key="dataProductOverview.shortKey" @click="onClickDataProductDetail(dataProductOverview.shortKey)">
-          <data-product-overview-card :data-product-overview="dataProductOverview" />
+      <v-col v-if="isLoading">
+        <div class="circle">
+          <v-progress-circular
+            :size="120"
+            indeterminate
+            color="white"
+          ></v-progress-circular>
+        </div>
+      </v-col>
+      <v-col
+        v-else-if="filteredDataProductsOverview.length > 0"
+        cols="12"
+        md="4"
+      >
+        <v-card
+          v-for="dataProductOverview in filteredDataProductsOverview"
+          :key="dataProductOverview.shortKey"
+          @click="onClickDataProductDetail(dataProductOverview.shortKey)"
+        >
+          <data-product-overview-card
+            :data-product-overview="dataProductOverview"
+          />
         </v-card>
+      </v-col>
+      <v-col v-else>
+        <p>No data products found.</p>
       </v-col>
     </v-row>
     <v-overlay v-if="openedDetails !== ''">
-      <data-product-detail-card v-click-outside="onClickOutsideDataProductDetail" :short-key="openedDetails" />
+      <data-product-detail-card
+        v-click-outside="onClickOutsideDataProductDetail"
+        :short-key="openedDetails"
+      />
     </v-overlay>
   </v-container>
 </template>
 
 <script>
-  import dataProductsOverviewProvider from "~/middleware/dataProductsOverviewProvider";
-  import dataProductImageProvider from "~/middleware/dataProductImageProvider";
+import dataProductsOverviewProvider from '~/middleware/dataProductsOverviewProvider'
+import dataProductImageProvider from '~/middleware/dataProductImageProvider'
 
-  export default
-  {
-    name: 'Marketplace',
-    data()
-    {
-      return {
-        search: '',
-        filter: '',
-        filters: ['All'],
-        dataProductsOverview:
-        [{
+export default {
+  name: 'Marketplace',
+  data() {
+    return {
+      search: '',
+      filter: '',
+      filters: ['All'],
+      dataProductsOverview: [
+        {
           shortKey: String,
           title: String,
           shortDescription: String,
           lastUpdated: Date,
           category: String,
           accessRight: String,
-          image: Uint8Array
-        }],
-        openedDetails: ''
-      }
-    },
-    async fetch()
-    {
-      this.dataProductsOverview = await this.fetchDataProductsOverview()
-      this.filters = this.getDataProductCategories()
-    },
-    computed:
-    {
-      filteredDataProductsOverview()
-      {
-        if (this.filter === '' || this.filter === 'All')
-        {
-          return this.dataProductsOverview;
-        }
-        else
-        {
-          return this.dataProductsOverview.filter(dataProduct =>
-              dataProduct.title.toLowerCase().includes(this.search.toLowerCase()) &&
-              dataProduct.category === this.filter
-          )
-        }
-      }
-    },
-    methods: {
-      async fetchDataProductsOverview()
-      {
-        const rawDataProductsOverview = await dataProductsOverviewProvider(this.$axios);
-        const dataProductsOverview = [];
-        for (const dataProduct of rawDataProductsOverview)
-        {
-          dataProductsOverview.push({
-            shortKey: dataProduct.shortKey,
-            title: dataProduct.title,
-            shortDescription: dataProduct.shortDescription,
-            lastUpdated: new Date(dataProduct.lastUpdated).toLocaleDateString("ge-GE"),
-            category: dataProduct.category,
-            accessRight: dataProduct.accessRight,
-            image: await dataProductImageProvider(this.$axios, dataProduct.shortKey)
-          });
-        }
-        return dataProductsOverview;
-      },
-      getDataProductCategories()
-      {
-        const categories = this.dataProductsOverview.map(dataProduct => dataProduct.category);
-        return this.filters.concat(Array.from(new Set(categories.map(category => category))));
-      },
-      onClickDataProductDetail(shortKey) {
-        this.openedDetails = shortKey
-      },
-      onClickOutsideDataProductDetail() {
-        this.openedDetails = ''
-      }
+          image: Uint8Array,
+        },
+      ],
+      openedDetails: '',
+      isLoading: false,
     }
-  };
+  },
+  async fetch() {
+    this.isLoading = true
+    this.dataProductsOverview = await this.fetchDataProductsOverview()
+    this.filters = this.getDataProductCategories()
+    this.isLoading = false
+  },
+  computed: {
+    filteredDataProductsOverview() {
+      if (this.filter === '' || this.filter === 'All') {
+        return this.dataProductsOverview
+      } else {
+        return this.dataProductsOverview.filter(
+          (dataProduct) =>
+            dataProduct.title
+              .toLowerCase()
+              .includes(this.search.toLowerCase()) &&
+            dataProduct.category === this.filter
+        )
+      }
+    },
+  },
+  methods: {
+    async fetchDataProductsOverview() {
+      const rawDataProductsOverview = await dataProductsOverviewProvider(
+        this.$axios
+      )
+      const dataProductsOverview = []
+      for (const dataProduct of rawDataProductsOverview) {
+        dataProductsOverview.push({
+          shortKey: dataProduct.shortKey,
+          title: dataProduct.title,
+          shortDescription: dataProduct.shortDescription,
+          lastUpdated: new Date(dataProduct.lastUpdated).toLocaleDateString(
+            'ge-GE'
+          ),
+          category: dataProduct.category,
+          accessRight: dataProduct.accessRight,
+          image: await dataProductImageProvider(
+            this.$axios,
+            dataProduct.shortKey
+          ),
+        })
+      }
+      return dataProductsOverview
+    },
+    getDataProductCategories() {
+      const categories = this.dataProductsOverview.map(
+        (dataProduct) => dataProduct.category
+      )
+      return this.filters.concat(
+        Array.from(new Set(categories.map((category) => category)))
+      )
+    },
+    onClickDataProductDetail(shortKey) {
+      this.openedDetails = shortKey
+    },
+    onClickOutsideDataProductDetail() {
+      this.openedDetails = ''
+    },
+  },
+}
 </script>
+
+<style>
+.circle {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70vh;
+}
+</style>
