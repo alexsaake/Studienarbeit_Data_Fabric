c4647454a022e7c25c94890da2b566cf038f00ea
alexander_scheffler <scheffleral86885@th-nuernberg.de>
2023-06-14 14:39:55 +0200

    GraphQl

---
+++ BE/src/test/java/com/mse/datafabric/GraphQlTest.java
@@ -0,0 +1,14 @@
+package com.mse.datafabric;
+
+import com.mse.datafabric.utils.RestClient;
+import org.junit.jupiter.api.Test;
+
+public class GraphQlTest {
+
+    @Test
+    public void testGraphQlPost(){
+        //String requestBody = "query GetImmobilien{getImmobilien{title}}";
+        //String response = RestClient.execute("https://localhost:8443/api/Gateway/DataProducts/immobilien/", "POST", 6000, "Content-Type: application/json", requestBody, null, null, true);
+        //System.out.println(response);
+    }
+}
