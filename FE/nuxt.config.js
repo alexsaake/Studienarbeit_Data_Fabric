c450ac897d10440661152ed62069aec106fd6635
alexander_scheffler <scheffleral86885@th-nuernberg.de>
2023-06-29 16:53:48 +0200

    https

--- FE/nuxt.config.js
+++ FE/nuxt.config.js
@@ -53,9 +53,9 @@ export default {
 
   proxy: {
     '/api': {
-      //target: 'http://localhost:8443',
+      target: 'http://localhost:8443',
       //target: 'http://127.0.0.1:8443',
-      target: 'http://192.168.178.11:8443',
+      //target: 'http://192.168.178.11:8443',
       //target: 'http://www.immofabric.de:8443',
       changeOrigin: true,
       secure: false,
