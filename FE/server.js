281ac062e6b65de16e199b3767cb30572f22b851
Dagreatpassi <pascal.soehnlein@gmail.com>
2023-06-29 20:35:41 +0200

    feat: add prod build and basicAuth

---
+++ FE/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const basicAuth = require('express-basic-auth');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const users = {
+  // Replace 'username' with your desired username and 'password' with your desired password
+  dataFabricUser: '123IHaveAccess',
+};
+
+app.use(
+  basicAuth({
+    users,
+    challenge: true,
+    unauthorizedResponse: 'Unauthorized',
+  })
+);
+
+// Proxy middleware
+app.use('/api', createProxyMiddleware({
+  target: 'http://localhost:8443', // Specify the target URL for proxying
+  changeOrigin: false, // Add the 'host' header to the proxied request
+}));
+
+app.use(express.static('dist'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`\n\n\n\n--------------------------------------------------`);
+  console.log(`Starting Data Fabric Prod server on port ${port}`);
+});
