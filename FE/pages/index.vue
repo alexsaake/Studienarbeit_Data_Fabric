f770dff3fe666c0d59241819a8cc6e57c7dc110b
Alexander Saake <alexandersaake@googlemail.com>
2023-08-02 19:39:47 +0200

    Added background image.

--- FE/pages/index.vue
+++ FE/pages/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="text-center">
-    <h1>Entdecke die Welt der Datenprodukte.</h1>
-    <p>Nutze das volle Potenzial der Daten.</p>
-    <p>Entdecke die Welt der Datenprodukte, konsumiere wertvolle Informationen und biete deine eigenen Daten an, um neue Möglichkeiten zu schaffen.</p>
-    <p>Willkommen bei DataFabric - Wo Daten zu Produkten werden.</p>
-    <v-img src="" />
-    <v-btn color="primary" href="/marketplace">Jetzt entdecken!</v-btn>
+    <v-img src="indexBackground.jpg">
+      <h1>Entdecke die Welt der Datenprodukte.</h1>
+      <p>Nutze das volle Potenzial der Daten.</p>
+      <p>Entdecke die Welt der Datenprodukte, konsumiere wertvolle Informationen und biete deine eigenen Daten an, um neue Möglichkeiten zu schaffen.</p>
+      <p>Willkommen bei DataFabric - Wo Daten zu Produkten werden.</p>
+      <v-btn color="primary" href="/marketplace">Jetzt entdecken!</v-btn>
+    </v-img>
   </div>
 </template>
