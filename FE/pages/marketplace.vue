0cbebc25ee90ef0cb872dd70e28b4c192e5b38f8
schne <schneiderha75561@th-nuernberg.de>
2023-08-07 19:56:07 +0200

    Update

--- FE/pages/marketplace.vue
+++ FE/pages/marketplace.vue
@@ -122,7 +122,7 @@
       },
       onCloseDataProduct()
       {
-        if(document.activeElement.tagName === 'BODY' && sessionStorage.getItem("datePickerOpen") === "false" || sessionStorage.getItem("datePickerOpen") === null)
+        if(document.activeElement.tagName === 'BODY' && sessionStorage.getItem("datePickerOpen") !== "true")
         {
           this.shortKey = '';
         }
