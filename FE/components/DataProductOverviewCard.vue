44aa3611904a4c0a082241a9668b28e7e902c465
tudor <ungureanutu82360@th-nuernberg.de>
2023-09-13 14:30:42 +0200

    Anzeige Durchschnittsbewertung (Nummer)

--- FE/components/DataProductOverviewCard.vue
+++ FE/components/DataProductOverviewCard.vue
@@ -16,7 +16,8 @@
         </v-row>
         <v-row class="mt-4" no-gutters>
           <v-col>Durschnittliche Bewertung</v-col>
-          <v-col><v-rating :value="averageRating" readonly></v-rating></v-col>
+          <v-col><pre>{{ averageRating.toFixed(2) }}</pre></v-col>
+          <v-col><v-rating :value="averageRating" readonly half-increments></v-rating></v-col>
         </v-row>
       </v-container>
     </v-card-text>
